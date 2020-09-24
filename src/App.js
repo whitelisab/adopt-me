f585ccaf5b0f5dbc5cce9ea3d56356ac7b43bb6a
Lisa White <whitelisab@gmail.com>
2020-09-24 16:49:55 -0600

    First commit, set up project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const Pet = () => {
+  return React.createElement("div", {}, React.createElement([
+    "h1",
+    {},
+    "Lundy"
+    )]
+  );
+}
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
