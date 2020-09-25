78b519adffe838211a65b29306736a33bbe66ca2
Lisa White <whitelisab@gmail.com>
2020-09-25 11:42:42 -0600

    Create reusable dropdown custom hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet.js";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Lundy" animal="Dog" breed="Border Terrier" />
-      <Pet name="Madison" animal="Dog" breed="Westie" />
-      <Pet name="Hawthorne" animal="Cat" breed="Orange Tabby" />
+      <SearchParams />
     </div>
   );
 };
