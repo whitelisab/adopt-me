52faca8f2573fc98b722f05a93da000a2753ec8e
Lisa White <whitelisab@gmail.com>
2020-09-24 19:25:38 -0600

    Add tools to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-const Pet = () => {
-  return React.createElement("div", {}, React.createElement([
-    "h1",
-    {},
-    "Lundy"
-    )]
-  );
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Lundy",
+      animal: "Dog",
+      breed: "Border Terrier",
+    }),
+    React.createElement(Pet, {
+      name: "Madison",
+      animal: "Dog",
+      breed: "Westie",
+    }),
+    React.createElement(Pet, {
+      name: "Hawthorne",
+      animal: "Cat",
+      breed: "Orange Tabby",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
